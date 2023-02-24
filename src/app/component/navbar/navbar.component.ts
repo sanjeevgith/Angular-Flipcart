1ce15c52889cfa6e3778a261a1802acaef5b3bc6
sanjeevgith <prajapatisanjiv8@gmail.com.com>
2023-02-24 10:30:35 +0530

    search added

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AddtobuyService } from 'src/app/services/addtobuy.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,9 +12,23 @@ export class NavbarComponent implements OnInit {
 
 
 
+  constructor(private addtobuy: AddtobuyService, private router: Router) { }
+
   counter: any
   ngOnInit(): void {
-   
+
+
+  }
+
+
+
+  search() {
+    var searchdata = (<HTMLInputElement>document.getElementById("searchinput")).value;
+    console.log(searchdata);
+
+    localStorage.setItem("searchdata", searchdata);
+    this.router.navigate(["productcategories"]);
+
   }
 
 
